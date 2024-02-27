4f544a88f7fda4a2f831de71690f250408fa82a2
Raya Chorbadzhiyska <rayach1512@gmail.com>
2024-02-27 10:55:38 +0200

    Display the position of the sensor on the board whenever the user hovers over it.

--- Views/HardwareSetupPage.xaml.cs
+++ Views/HardwareSetupPage.xaml.cs
@@ -1,4 +1,8 @@
-﻿using System.Windows.Controls;
+﻿using SensorCalibrationSystem.Models;
+using System.Windows.Controls;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
 
 namespace SensorCalibrationSystem.Views
 {
@@ -7,9 +11,38 @@ namespace SensorCalibrationSystem.Views
     /// </summary>
     public partial class HardwareSetupPage : Page
     {
+        private const string appResourcesPath = @"pack://application:,,,/SensorCalibrationSystem;component/Resources";
+
         public HardwareSetupPage()
         {
             InitializeComponent();
+        }
+
+        private void SensorsDisplay_MouseMove(object sender, MouseEventArgs e)
+        {
+            var item = VisualTreeHelper.HitTest(SensorsDisplay, Mouse.GetPosition(SensorsDisplay)).VisualHit;
+
+            // find ListViewItem (or null)
+            while (item != null && !(item is ListBoxItem))
+                item = VisualTreeHelper.GetParent(item);
+
+            if (item is not null)
+            {
+                SensorModel sensor = ((ListBoxItem)item).DataContext as SensorModel;
+
+                if (sensor is null)
+                {
+                    return;
+                }
+
+                string imageName = $"{sensor.Name}-on-board.png";
+
+                BoardDisplay.Source = new BitmapImage(new System.Uri($"{appResourcesPath}/{imageName}"));
+            }
+            else
+            {
+                BoardDisplay.Source = new BitmapImage(new System.Uri($"{appResourcesPath}/board-top.png"));
+            }
         }
     }
 }
