fe165ab67c9a61948350eb51e145ecc1fb4f1c4c
Raya Chorbadzhiyska <rayach1512@gmail.com>
2024-03-26 21:22:52 +0200

    Implement a custom window title bar.

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using SensorCalibrationSystem.Contracts;
 using SensorCalibrationSystem.ViewModels;
+using System;
 using System.Windows;
 
 namespace SensorCalibrationSystem
@@ -17,11 +18,61 @@ namespace SensorCalibrationSystem
             DataContext = viewModel;
 
             this.boardCommunicationService = boardCommunicationService;
+
+            StateChanged += OnWindowStateChanged;
+
+            MaxHeight = SystemParameters.VirtualScreenHeight;
+            MaxWidth = SystemParameters.VirtualScreenWidth;
         }
 
         private void Window_Closing(object sender, System.ComponentModel.CancelEventArgs e)
         {
             boardCommunicationService.Disconnect();
         }
+
+        #region Toolbar
+
+        private void OnMinimizeButtonClick(object sender, RoutedEventArgs e)
+        {
+            WindowState = WindowState.Minimized;
+        }
+
+        private void OnMaximizeRestoreButtonClick(object sender, RoutedEventArgs e)
+        {
+            if (WindowState == WindowState.Maximized)
+            {
+                WindowState = WindowState.Normal;
+            }
+            else
+            {
+                WindowState = WindowState.Maximized;
+            }
+        }
+
+        private void OnCloseButtonClick(object sender, RoutedEventArgs e)
+        {
+            Close();
+        }
+
+        private void RefreshMaximizeRestoreButton()
+        {
+            if (WindowState == WindowState.Maximized)
+            {
+                maximizeButton.Visibility = Visibility.Collapsed;
+                restoreButton.Visibility = Visibility.Visible;
+            }
+            else
+            {
+                maximizeButton.Visibility = Visibility.Visible;
+                restoreButton.Visibility = Visibility.Collapsed;
+            }
+        }
+
+        private void OnWindowStateChanged(object? sender, EventArgs e)
+        {
+            RefreshMaximizeRestoreButton();
+        }
+
+        #endregion
     }
 }
